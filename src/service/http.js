71f9ee57dd1fafae1448d6502bd5c2a87eadfe05
mfiim <fajar12345678905@gmail.com>
2023-11-30 19:32:00 +0700

    Update log api expired fix

--- src/service/http.js
+++ src/service/http.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const API_KEY = "49cfda4ac2mshce33f289e3c4c15p171c0ejsn1cc7bbf21637";
+const BASE_URL = "https://priceline-com-provider.p.rapidapi.com";
+
 const baseUrl = process.env.REACT_APP_BASE_API_URL;
 const apiKey = process.env.REACT_APP_API_KEY;
 const apiHost = process.env.REACT_APP_API_HOST;
@@ -7,8 +10,8 @@ const apiHost = process.env.REACT_APP_API_HOST;
 const http = axios.create({
   baseURL: baseUrl,
   headers: {
-    "x-rapidapi-host": apiHost,
-    "x-rapidapi-key": apiKey,
+   "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Host": "priceline-com-provider.p.rapidapi.com"
   },
 });
 
